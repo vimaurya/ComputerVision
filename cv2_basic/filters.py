510c9e60c332d14f28589fb2bea29c14bac3415a
vikash maurya <mauryavikashhere@gmail.com>
2024-06-13 14:26:37 +0530

    testing

---
+++ cv2_basic/filters.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+e
+image = cv2.imread('../data/bear.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+gaussian_blur = cv2.GaussianBlur(image, (5, 5), 0)
+
+sobel_x = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+sobel_y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+sobel_combined = cv2.magnitude(sobel_x, sobel_y)
+
+laplacian = cv2.Laplacian(image, cv2.CV_64F)
+
+median = cv2.medianBlur(image, 5)
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(231), plt.imshow(image), plt.title('Original')
+plt.subplot(232), plt.imshow(gaussian_blur), plt.title('Gaussian Blur')
+plt.subplot(233), plt.imshow(sobel_combined, cmap='gray'), plt.title('Sobel Edge Detection')
+plt.subplot(234), plt.imshow(laplacian, cmap='gray'), plt.title('Laplacian Edge Detection')
+plt.subplot(235), plt.imshow(median), plt.title('Median Filter')
+
+plt.show()
+
+#%%
--- image_classification/test.py
+++ image_classification/test.py
@@ -2,6 +2,7 @@ import torch
 from helper_functions import predict_weather
 from helper_functions import weatherClassificationModelv0 as wc0
 from helper_functions import weatherClassificationModelv1 as wc1
+from helper_functions import weatherClassificationModelv2 as wc2
 
 
 modelv0 = wc0()
@@ -10,13 +11,17 @@ modelv0.load_state_dict(torch.load('../models/weather_classification_modelv0.pth
 modelv1 = wc1()
 modelv1.load_state_dict(torch.load('../models/weather_classification_modelv1.pth'))
 
+modelv2 = wc2()
+modelv2.load_state_dict(torch.load('../models/weather_classification_modelv2.pth'))
 
-img_class = 'sunrise'
 
-img_num = 11
+img_class = 'shine'
+
+img_num = 66
 
 print(f"modelv0 : {predict_weather(modelv0, img_class, img_num)}")
 print(f"modelv1 : {predict_weather(modelv1, img_class, img_num)}")
+print(f"modelv2 : {predict_weather(modelv2, img_class, img_num)}")
 
 
 #%%
