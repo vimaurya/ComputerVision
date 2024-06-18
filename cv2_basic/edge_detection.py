68940fc8d67f81c5e11785672629e6c6ab3c2254
vikash maurya <mauryavikashhere@gmail.com>
2024-06-18 20:04:33 +0530

    boundary detection

--- cv2_basic/edge_detection.py
+++ cv2_basic/edge_detection.py
@@ -8,11 +8,19 @@ img = cv.imread(os.path.join('..','data','kobe.jpg'))
 
 img_edge = cv.Canny(img, 100,200)
 
-img_edge_dilate = cv.dilate(img_edge, np.ones((3,3), dtype=np.int8), iterations=1)
 
-img_edge_erode = cv.erode(img_edge_dilate, np.ones((3,3), dtype=np.int8), iterations=1)
+kernel = np.array([[0,1,0],
+                   [1,1,1],
+                   [0,1,0]], dtype=np.uint8)
 
-cv.imshow('img',img)
+
+#kernel = np.ones((3,3), dtype=np.uint8)
+
+img_edge_dilate = cv.dilate(img_edge, kernel, iterations=1)
+
+img_edge_erode = cv.erode(img_edge_dilate, kernel, iterations=1)
+
+#cv.imshow('img',img)
 cv.imshow('img_edge_erode',img_edge_erode)
 cv.imshow('img_edge', img_edge)
 cv.imshow('img_dilate', img_edge_dilate)
