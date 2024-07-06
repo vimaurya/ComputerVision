6f7e6609e6f3d53e2a8e99fa316b3cdbe9685aa4
vikash maurya <mauryavikashhere@gmail.com>
2024-07-06 18:56:30 +0530

    testing

--- test.py
+++ test.py
@@ -1,33 +1,26 @@
-import cv2
+import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
 
-image_path = 'data/kobe.jpg'
-color_image = cv2.imread(image_path)
+img_path = 'data/faces.jpg'
 
-hsv_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+img = cv.imread(img_path)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-h, s, v = cv2.split(hsv_image)
-v_dilated = cv2.dilate(v, kernel, iterations=1)
-v_eroded = cv2.erode(v, kernel, iterations=1)
+kernel1 = np.array([[1, 1, 1],
+                    [0, 0, 0],
+                    [-1, -1, -1]], np.float32)
 
-hsv_dilated = cv2.merge([h, s, v_dilated])
-hsv_eroded = cv2.merge([h, s, v_eroded])
+kernel2 = np.array([[1, 0, -1],
+                    [1, 0, -1],
+                    [1, 0, -1]], np.float32)
 
-dilated_color_image = cv2.cvtColor(hsv_dilated, cv2.COLOR_HSV2BGR)
-eroded_color_image = cv2.cvtColor(hsv_eroded, cv2.COLOR_HSV2BGR)
+horizontal = cv.filter2D(img_gray, 0, kernel1)
 
-titles = ['Original Color Image', 'Dilated Color Image (V Channel)', 'Eroded Color Image (V Channel)']
-images = [color_image, dilated_color_image, eroded_color_image]
+vertical = cv.filter2D(img_gray, 0, kernel2)
 
-plt.figure(figsize=(15, 5))
-for i in range(3):
-    plt.subplot(1, 3, i+1)
-    plt.imshow(cv2.cvtColor(images[i], cv2.COLOR_BGR2RGB))
-    plt.title(titles[i])
-    plt.axis('off')
-
-plt.tight_layout()
-plt.show()
+cv.imshow('original', img_gray)
+cv.imshow('filter', horizontal)
+cv.imshow('filter_vertical', vertical)
+cv.waitKey(0)
+cv.destroyAllWindows()
